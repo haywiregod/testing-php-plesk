3951d82758974912ce353fab61d6849d7b4c239d
haywiregod <vickytiwari765@gmail.com>
2020-12-18 22:46:41 +0530

    added db connection

---
+++ db.php
@@ -0,0 +1,31 @@
+<?php
+if($_SERVER['HTTP_HOST']=='www.thecassava.co' || $_SERVER['HTTP_HOST']=='thecassava.co'){
+  // production server
+  // define("SERVER_TYPE","PROD");
+  $servername = "localhost:3306";
+  $username = "superuser";
+  $password = "V84_wnqCM4ocfjzv";
+  $db="cassava_prod_";
+}
+else{
+  //development server
+  // define("SERVER_TYPE","DEV");
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $db = "cassava";
+}
+
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$db);
+
+// Check connection
+if ($conn->connect_error) {
+ die("Connection failed: " . $conn->connect_error);
+}
+
+date_default_timezone_set('Asia/Kolkata');
+
+?>
