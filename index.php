79e92876aa0b57917920fa133cc08254afedc869
haywiregod <vickytiwari765@gmail.com>
2020-12-19 18:14:43 +0530

    added csv output

--- index.php
+++ index.php
@@ -2,7 +2,26 @@
 header('Content-Type: application/json');
 include('includes/db.php');
 include('product/ProductClass.php');
-$pid = $_GET['pid'];
-$product = new Product($pid);
-echo json_encode($product->get_info());
+$result = $conn->query('SELECT * FROM `products`'); 
+if (!$result) die('Couldn\'t fetch records'); 
+$num_fields = mysqli_num_fields($result); 
+$headers = array(); 
+for ($i = 0; $i < $num_fields; $i++) 
+{     
+       $headers[] = mysqli_field_name($result , $i); 
+} 
+$fp = fopen('php://output', 'w'); 
+if ($fp && $result) 
+{     
+       header('Content-Type: text/csv');
+       header('Content-Disposition: attachment; filename="export.csv"');
+       header('Pragma: no-cache');    
+       header('Expires: 0');
+       fputcsv($fp, $headers); 
+       while ($row = mysqli_fetch_row($result)) 
+       {
+          fputcsv($fp, array_values($row)); 
+       }
+die; 
+} 
 ?>
